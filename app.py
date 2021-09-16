5f96e996b366bd9240825d58164d8497a079f1da
Sergej Steinle <steinle.sergej.prog@gmail.com>
2021-09-16 17:09:40 +0200

    Init Project

---
+++ app.py
@@ -0,0 +1,23 @@
+import tkinter
+
+
+#define window
+root = tkinter.Tk()
+root.title("Checklist")
+root.iconbitmap("checklist_icon.ico")
+root.geometry("400x400")
+root.resizable(0,0)
+
+#define fonts and colors
+main_font = ("Times New Roman",12)
+main_color = "#BBA191"
+btn_color = "#DDD0C8"
+
+root.config(bg=main_color)
+
+#define functions
+
+
+#define layout
+
+root.mainloop()
